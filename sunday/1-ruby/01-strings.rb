1b700fda7760f78417e54958f12b30650f5a2fa1
Hartley Brody <hartley.brody@gmail.com>
2016-12-13 10:13:40 -0500

    clear out all of the ruby stuff

--- sunday/1-ruby/01-strings.rb
+++
@@ -1,7 +0,0 @@
-# 1. Working with strings
-
-# todo:
-# change the message that is printed to the console when this script is run
-# print out a word that gets passed in, using ARGV[0]
-# use string interpolation to put a name inside a message
-puts "Hello there."
--- sunday/1-ruby/02-objects.rb
+++
@@ -1,6 +0,0 @@
-# 2. Working with objects
-
-# todo:
-# call the .strftime() method and make the output prettier
-# hint: http://apidock.com/ruby/DateTime/strftime
-puts Time.now
